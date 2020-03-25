0f7c4b11924e98af49698f3205809a0770d1fe11
刘移志 <liuyizhi@bond520.com>
2020-03-25 17:05:41 +0800

    commit

--- ios/Classes/FluttervideocompPlugin.m
+++ ios/Classes/FluttervideocompPlugin.m
@@ -1,6 +1,6 @@
 #import "FluttervideocompPlugin.h"
 
-#import <flutter_video_comp/fluttervideocomp-Swift.h>
+#import <flutter_video_comp/flutter_video_comp-Swift.h>
 
 @implementation FluttervideocompPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
